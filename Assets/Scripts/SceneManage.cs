f55012ca77d40d928590ba362e8cc0b53280acf1
Tristan247 <56597679+Tristan247@users.noreply.github.com>
2021-02-12 14:03:19 +0100

    RaceTrack
    Added 3 scenes with different race tracks where the player can drive around

---
+++ Assets/Scripts/SceneManage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //when the player hits the collider it loads next scene
+        SceneManager.LoadScene("Track2");
+    }
+}
---
+++ Assets/Scripts/SceneManage2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage2 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //when the player hits the collider it loads next scene
+        SceneManager.LoadScene("Track3");
+    }
+}
+
